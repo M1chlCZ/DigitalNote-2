d51794172d56c0bd10106f35a51defd167ac1b45
Ludo Ruisch <admin@ludoruisch.nl>
2021-07-18 03:10:35 +0200

    patch: align AcceptBlock function with tabs.

--- src/cblock.cpp
+++ src/cblock.cpp
@@ -1478,102 +1478,101 @@ bool CBlock::CheckBlock(bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSig) c
 
 bool CBlock::AcceptBlock()
 {
-    AssertLockHeld(cs_main);
-	
-    // Check for duplicate
-    uint256 hash = GetHash();
-    if (mapBlockIndex.count(hash))
-	{
-        return error("AcceptBlock() : block already in mapBlockIndex");
-	}
-	
-    // Get prev block index
-    std::map<uint256, CBlockIndex*>::iterator mi = mapBlockIndex.find(hashPrevBlock);
-    if (mi == mapBlockIndex.end())
-	{
-        return DoS(10, error("AcceptBlock() : prev block not found"));
-    }
-	
-	CBlockIndex* pindexPrev = (*mi).second;
-    int nHeight = pindexPrev->nHeight+1;
-	
-    // Check created block for version control
-    if (nVersion < 7)
-	{
-        return DoS(100, error("AcceptBlock() : reject too old nVersion = %d", nVersion));
-	}
-    else if (nVersion > 7)
-	{
-        return DoS(100, error("AcceptBlock() : reject too new nVersion = %d", nVersion));
-	}
-	
-    // Check block against Velocity parameters
-    if(Velocity_check(nHeight))
-    {
-        // Announce Velocity constraint failure
-        if(!Velocity(pindexPrev, this))
-        {
-            return DoS(100, error("AcceptBlock() : Velocity rejected block %d, required parameters not met", nHeight));
-        }
-    }
+	AssertLockHeld(cs_main);
 
-    uint256 hashProof;
-    if (IsProofOfWork() && nHeight > Params().EndPoWBlock())
+	// Check for duplicate
+	uint256 hash = GetHash();
+	if (mapBlockIndex.count(hash))
 	{
-        return DoS(100, error("AcceptBlock() : reject proof-of-work at height %d", nHeight));
-    }
+		return error("AcceptBlock() : block already in mapBlockIndex");
+	}
+
+	// Get prev block index
+	std::map<uint256, CBlockIndex*>::iterator mi = mapBlockIndex.find(hashPrevBlock);
+	if (mi == mapBlockIndex.end())
+	{
+		return DoS(10, error("AcceptBlock() : prev block not found"));
+	}
+
+	CBlockIndex* pindexPrev = (*mi).second;
+	int nHeight = pindexPrev->nHeight+1;
+
+	// Check created block for version control
+	if (nVersion < 7)
+	{
+		return DoS(100, error("AcceptBlock() : reject too old nVersion = %d", nVersion));
+	}
+	else if (nVersion > 7)
+	{
+		return DoS(100, error("AcceptBlock() : reject too new nVersion = %d", nVersion));
+	}
+
+	// Check block against Velocity parameters
+	if(Velocity_check(nHeight))
+	{
+		// Announce Velocity constraint failure
+		if(!Velocity(pindexPrev, this))
+		{
+			return DoS(100, error("AcceptBlock() : Velocity rejected block %d, required parameters not met", nHeight));
+		}
+	}
+
+	uint256 hashProof;
+	if (IsProofOfWork() && nHeight > Params().EndPoWBlock())
+	{
+		return DoS(100, error("AcceptBlock() : reject proof-of-work at height %d", nHeight));
+	}
 	else
 	{
-        // PoW is checked in CheckBlock()
-        if (IsProofOfWork())
-        {
-            hashProof = GetPoWHash();
-        }
-    }
+		// PoW is checked in CheckBlock()
+		if (IsProofOfWork())
+		{
+			hashProof = GetPoWHash();
+		}
+	}
 
-    if (IsProofOfStake() && nHeight < Params().StartPoSBlock())
+	if (IsProofOfStake() && nHeight < Params().StartPoSBlock())
 	{
-        return DoS(100, error("AcceptBlock() : reject proof-of-stake at height <= %d", nHeight));
+		return DoS(100, error("AcceptBlock() : reject proof-of-stake at height <= %d", nHeight));
 	}
-	
-    // Check coinbase timestamp
-    if (GetBlockTime() > FutureDrift((int64_t)vtx[0].nTime) && IsProofOfStake())
+
+	// Check coinbase timestamp
+	if (GetBlockTime() > FutureDrift((int64_t)vtx[0].nTime) && IsProofOfStake())
 	{
-        return DoS(50, error("AcceptBlock() : coinbase timestamp is too early"));
+		return DoS(50, error("AcceptBlock() : coinbase timestamp is too early"));
 	}
-	
-    // Check coinstake timestamp
-    if (IsProofOfStake() && !CheckCoinStakeTimestamp(nHeight, GetBlockTime(), (int64_t)vtx[1].nTime))
+
+	// Check coinstake timestamp
+	if (IsProofOfStake() && !CheckCoinStakeTimestamp(nHeight, GetBlockTime(), (int64_t)vtx[1].nTime))
 	{
-        return DoS(50, error("AcceptBlock() : coinstake timestamp violation nTimeBlock=%d nTimeTx=%u", GetBlockTime(), vtx[1].nTime));
+		return DoS(50, error("AcceptBlock() : coinstake timestamp violation nTimeBlock=%d nTimeTx=%u", GetBlockTime(), vtx[1].nTime));
 	}
-	
-    // Check proof-of-work or proof-of-stake
+
+	// Check proof-of-work or proof-of-stake
 	/*
 		The following block has this case:
 			46921, 46923, 46924
 	*/
-    if (nHeight != 46921 && nHeight != 46923 && nHeight != 46924 && nBits != GetNextTargetRequired(pindexPrev, IsProofOfStake()))
+	if (nHeight != 46921 && nHeight != 46923 && nHeight != 46924 && nBits != GetNextTargetRequired(pindexPrev, IsProofOfStake()))
 	{
-        return DoS(100, error("AcceptBlock() : incorrect %s", IsProofOfWork() ? "proof-of-work" : "proof-of-stake"));
+		return DoS(100, error("AcceptBlock() : incorrect %s", IsProofOfWork() ? "proof-of-work" : "proof-of-stake"));
 	}
-	
-    // Check timestamp against prev
-    if (GetBlockTime() <= pindexPrev->GetPastTimeLimit() || FutureDrift(GetBlockTime()) < pindexPrev->GetBlockTime())
+
+	// Check timestamp against prev
+	if (GetBlockTime() <= pindexPrev->GetPastTimeLimit() || FutureDrift(GetBlockTime()) < pindexPrev->GetBlockTime())
 	{
-        return error("AcceptBlock() : block's timestamp is too early");
+		return error("AcceptBlock() : block's timestamp is too early");
 	}
-	
-    // Check that all transactions are finalized
-    for(const CTransaction& tx : vtx)
-    {
-        if (!IsFinalTx(tx, nHeight, GetBlockTime()))
+
+	// Check that all transactions are finalized
+	for(const CTransaction& tx : vtx)
+	{
+		if (!IsFinalTx(tx, nHeight, GetBlockTime()))
 		{
-            return DoS(10, error("AcceptBlock() : contains a non-final transaction"));
-        }
-    }
+			return DoS(10, error("AcceptBlock() : contains a non-final transaction"));
+		}
+	}
 	
-	//if(nHeight > 394623)
 	/*
 		Bad blocks:
 			130942
@@ -1638,71 +1637,71 @@ bool CBlock::AcceptBlock()
 		}
 	}
 	
-    // Check that the block chain matches the known block chain up to a checkpoint
-    if (!Checkpoints::CheckHardened(nHeight, hash))
+	// Check that the block chain matches the known block chain up to a checkpoint
+	if (!Checkpoints::CheckHardened(nHeight, hash))
 	{
-        return DoS(100, error("AcceptBlock() : rejected by hardened checkpoint lock-in at %d", nHeight));
+		return DoS(100, error("AcceptBlock() : rejected by hardened checkpoint lock-in at %d", nHeight));
 	}
-	
-    // Verify hash target and signature of coinstake tx
-    if (IsProofOfStake())
-    {
-        uint256 targetProofOfStake;
-        if (!CheckProofOfStake(pindexPrev, vtx[1], nBits, hashProof, targetProofOfStake))
-        {
-            return error("AcceptBlock() : check proof-of-stake failed for block %s", hash.ToString());
-        }
-    }
 
-    // Check that the block satisfies synchronized checkpoint
-    if (!Checkpoints::CheckSync(nHeight))
+	// Verify hash target and signature of coinstake tx
+	if (IsProofOfStake())
 	{
-        return error("AcceptBlock() : rejected by synchronized checkpoint");
+		uint256 targetProofOfStake;
+		if (!CheckProofOfStake(pindexPrev, vtx[1], nBits, hashProof, targetProofOfStake))
+		{
+			return error("AcceptBlock() : check proof-of-stake failed for block %s", hash.ToString());
+		}
 	}
-	
-    // Enforce rule that the coinbase starts with serialized block height
-    CScript expect = CScript() << nHeight;
-    if (
+
+	// Check that the block satisfies synchronized checkpoint
+	if (!Checkpoints::CheckSync(nHeight))
+	{
+		return error("AcceptBlock() : rejected by synchronized checkpoint");
+	}
+
+	// Enforce rule that the coinbase starts with serialized block height
+	CScript expect = CScript() << nHeight;
+	if (
 		vtx[0].vin[0].scriptSig.size() < expect.size() ||
-        !std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin())
+		!std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin())
 	)
 	{
-        return DoS(100, error("AcceptBlock() : block height mismatch in coinbase"));
+		return DoS(100, error("AcceptBlock() : block height mismatch in coinbase"));
 	}
-	
-    // Write block to history file
-    if (!CheckDiskSpace(::GetSerializeSize(*this, SER_DISK, CLIENT_VERSION)))
+
+	// Write block to history file
+	if (!CheckDiskSpace(::GetSerializeSize(*this, SER_DISK, CLIENT_VERSION)))
 	{
-        return error("AcceptBlock() : out of disk space");
+		return error("AcceptBlock() : out of disk space");
 	}
-	
-    unsigned int nFile = -1;
-    unsigned int nBlockPos = 0;
-    if (!WriteToDisk(nFile, nBlockPos))
+
+	unsigned int nFile = -1;
+	unsigned int nBlockPos = 0;
+	if (!WriteToDisk(nFile, nBlockPos))
 	{
-        return error("AcceptBlock() : WriteToDisk failed");
+		return error("AcceptBlock() : WriteToDisk failed");
 	}
-	
-    if (!AddToBlockIndex(nFile, nBlockPos, hashProof))
+
+	if (!AddToBlockIndex(nFile, nBlockPos, hashProof))
 	{
-        return error("AcceptBlock() : AddToBlockIndex failed");
+		return error("AcceptBlock() : AddToBlockIndex failed");
 	}
-	
-    // Relay inventory, but don't relay old inventory during initial block download
-    int nBlockEstimate = Checkpoints::GetTotalBlocksEstimate();
-    if (hashBestChain == hash)
-    {
-        LOCK(cs_vNodes);
-        for(CNode* pnode : vNodes)
+
+	// Relay inventory, but don't relay old inventory during initial block download
+	int nBlockEstimate = Checkpoints::GetTotalBlocksEstimate();
+	if (hashBestChain == hash)
+	{
+		LOCK(cs_vNodes);
+		for(CNode* pnode : vNodes)
 		{
-            if (nBestHeight > (pnode->nStartingHeight != -1 ? pnode->nStartingHeight - 2000 : nBlockEstimate))
+			if (nBestHeight > (pnode->nStartingHeight != -1 ? pnode->nStartingHeight - 2000 : nBlockEstimate))
 			{
-                pnode->PushInventory(CInv(MSG_BLOCK, hash));
+				pnode->PushInventory(CInv(MSG_BLOCK, hash));
 			}
 		}
-    }
+	}
 
-    return true;
+	return true;
 }
 
 #ifdef ENABLE_WALLET
